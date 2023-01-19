d555b738a870e0f9575f84fcef010dbd41684d44
Higor Nunes <33237547+HigorNunesM@users.noreply.github.com>
2023-01-19 11:02:41 +0100

    Update ec2_test.py

--- ec2_test.py
+++ ec2_test.py
@@ -2,12 +2,12 @@
 import pandas as pd
 import random
 
-print(Starting...")
+print("Starting...")
 # Create a random pandas dataframe
 df = pd.DataFrame({"A": [random.randint(0,100) for i in range(10)],
                   "B": [random.randint(0,100) for i in range(10)],
                   "C": [random.randint(0,100) for i in range(10)]})
 print(df)
 # Save the dataframe as a csv file
-df.to_csv('/home/ec2-user/project/output.csv', index=False)
+df.to_csv('output.csv', index=False)
 print("File saved successfully")
