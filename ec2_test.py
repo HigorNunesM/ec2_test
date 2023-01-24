dfd1254003ac2ee0d12bdefb1afb7c6aa600eeb0
Higor Nunes <33237547+HigorNunesM@users.noreply.github.com>
2023-01-24 18:15:01 +0100

    Update ec2_test.py

--- ec2_test.py
+++ ec2_test.py
@@ -9,5 +9,6 @@ df = pd.DataFrame({"A": [random.randint(0,100) for i in range(10)],
                   "C": [random.randint(0,100) for i in range(10)]})
 print(df)
 # Save the dataframe as a csv file
-df.to_csv('output.csv', index=False)
+#df.to_csv('output.csv', index=False)
+df.to_csv('s3://sim-project/Project_0001/output/output.csv', index=False)
 print("File saved successfully")
